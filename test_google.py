bd2fadf6eeb78d370d268e363885a363f763ae5a
Jean-90-Prueba <kardhidem@gmail.com>
2025-07-07 14:25:43 -0500

    Update test_google.py

--- test_google.py
+++ test_google.py
@@ -1,25 +1,32 @@
-import os
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-desired_cap = {
-    'browserName': 'Chrome',
-    'browserVersion': 'latest',
-    'bstack:options': {
-        'os': 'Windows',
-        'osVersion': '10',
-        'projectName': 'Prueba BrowserStack',
-        'buildName': 'Build 1',
-        'sessionName': 'Validar título Google',
-        'local': 'false',
-        'seleniumVersion': '4.0.0',
-        'userName': os.environ.get('BROWSERSTACK_USERNAME'),
-        'accessKey': os.environ.get('BROWSERSTACK_ACCESS_KEY')
-    }
+# ✅ Datos de acceso
+BROWSERSTACK_USERNAME = 'kardhidem_QrP5u4'
+BROWSERSTACK_ACCESS_KEY = 'qFxQwJxqHWAyXHABR9S7'
+
+# ✅ Capabilities
+bstack_options = {
+    'os': 'Windows',
+    'osVersion': '10',
+    'projectName': 'Prueba BrowserStack',
+    'buildName': 'Build 1',
+    'sessionName': 'Validar título Google',
+    'seleniumVersion': '4.11.0',
+    'local': 'false',
+    'userName': BROWSERSTACK_USERNAME,
+    'accessKey': BROWSERSTACK_ACCESS_KEY
 }
 
+options = Options()
+options.set_capability('browserName', 'Chrome')
+options.set_capability('browserVersion', 'latest')
+options.set_capability('bstack:options', bstack_options)
+
+# ✅ Ejecutar prueba
 driver = webdriver.Remote(
     command_executor='https://hub.browserstack.com/wd/hub',
-    desired_capabilities=desired_cap
+    options=options
 )
 
 driver.get("https://www.google.com")
